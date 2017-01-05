f3e28ad8208ff1191adf35d8491cdaba6e84720e
JGuiraud <jerome31guiraud@gmail.com>
2017-01-05 13:44:10 +0100

    commit

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('have fun !');
+$("input").focusout(function(){
+
+	var prenom = $("#first_name").val();
+	var nom = $("#last_name").val();
+	var ville = $("#city").val();
+
+		$("#prenom").html(prenom);
+		$("#nom").html(nom);
+		$("#ville").html(ville); 
+
+		if (prenom == "" || nom == "") {
+			alert("vous n'avez pas rempli tous les champs")
+		} else {
+				$("span#username").html(prenom + " " + nom);
+		}
+});
